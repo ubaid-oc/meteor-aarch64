fff21d1f6ebf702bda6598aec9d021adae621f67
shivendoodeshmukh <shivendoo.deshmukh@aventior.com>
2023-07-01 02:29:23 +0530

    Update generate-dev-bundle.sh

--- scripts/generate-dev-bundle.sh
+++ scripts/generate-dev-bundle.sh
@@ -14,8 +14,7 @@ source "$(dirname $0)/build-dev-bundle-common.sh"
 echo CHECKOUT DIR IS "$CHECKOUT_DIR"
 echo BUILDING DEV BUNDLE "$BUNDLE_VERSION" IN "$DIR"
 
-export DIR="/tmp"
-cd /tmp
+cd "$DIR"
 
 extractNodeFromTarGz() {
     LOCAL_TGZ="${CHECKOUT_DIR}/node_${PLATFORM}_v${NODE_VERSION}.tar.gz"
